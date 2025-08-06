79c06d37742b3cd1fedbeddd9825daf836dddff6
Koustubh Karande <karandekoustubh9@gmail.com>
2025-08-06 17:27:18 +0530

    FullSize Loader Changes

--- frontend/src/components/FullSizeLoader.jsx
+++ frontend/src/components/FullSizeLoader.jsx
@@ -3,16 +3,12 @@ import BarsLoader from "./BarLoader"
 
 export default function FullSizeLoader() {
   useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflow = "hidden"
-    } else {
-      document.body.style.overflow = ""
-    }
+    document.body.style.overflow = "hidden"
 
     return () => {
       document.body.style.overflow = ""
     }
-  }, [isOpen])
+  }, [])
   return (
     <div className="absolute top-0 left-0 w-screen z-[100] h-screen flex items-center justify-center bg-background/50 backdrop-blur-md pointer-events-none">
       <BarsLoader />
