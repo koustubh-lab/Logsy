7c83766c54a40f4a03ae2d6db030492f8a797cae
Koustubh Karande <karandekoustubh9@gmail.com>
2025-08-08 00:07:13 +0530

    Minor Changes

--- backend/src/main/java/com/spring/blog_application/controller/LoginWithMagicLinkController.java
+++ backend/src/main/java/com/spring/blog_application/controller/LoginWithMagicLinkController.java
@@ -48,7 +48,7 @@ public class LoginWithMagicLinkController {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User with email: " + request.email() + " was not found");
             }
 
-            if (userService.isUserAccountActivated(request.email())) {
+            if (!userService.isUserAccountActivated(request.email())) {
                 return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User's account is not activated");
             }
 
